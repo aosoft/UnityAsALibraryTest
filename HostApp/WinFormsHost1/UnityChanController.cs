09702fa9c422d508e737ed3b68dae605968d7c74
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2020-06-23 08:37:25 +0900

    Commit

---
+++ HostApp/WinFormsHost1/UnityChanController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MagicOnion.Server.Hubs;
+using UnityLibrary.Server.Hubs;
+
+namespace WinFormsHost1
+{
+    class UnityChanController : StreamingHubBase<IUnityChanController, IUnityChanControllerReceiver>, IUnityChanController
+    {
+        private IGroup _group;
+
+        public void SetAnimation(AnimeType animeType)
+        {
+            Broadcast(_group).SetAnimation(animeType);
+        }
+
+        public void SetMessageText(string msg)
+        {
+            Broadcast(_group).SetMessageText(msg);
+        }
+
+        async Task IUnityChanController.Register()
+        {
+            _group = await Group.AddAsync("UnityLibrary");
+            Current = this;
+        }
+
+        Task IUnityChanController.Unregister()
+        {
+            Current = null;
+            return _group?.RemoveAsync(Context).AsTask();
+        }
+
+        public static UnityChanController Current { get; private set; }
+    }
+}
